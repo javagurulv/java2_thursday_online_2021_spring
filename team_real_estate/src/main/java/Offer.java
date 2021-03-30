9fd7b939e17924720da2817f11340767843ae193
ViktorChesnokov <doc18@inbox.lv>
2021-03-30 21:34:24 +0300

    Created : User/Offer classes

---
+++ team_real_estate/src/main/java/Offer.java
@@ -0,0 +1,63 @@
+import java.util.Objects;
+
+class Offer {
+    String offerType;
+    String propertyCategory;
+    String description;
+    double price;
+
+    public Offer(String offerType, String propertyCategory, String description, double price) {
+        this.offerType = offerType;
+        this.propertyCategory = propertyCategory;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getOfferType() {
+        return offerType;
+    }
+
+    public void setOfferType(String offerType) {
+        this.offerType = offerType;
+    }
+
+    public String getPropertyCategory() {
+        return propertyCategory;
+    }
+
+    public void setPropertyCategory(String propertyCategory) {
+        this.propertyCategory = propertyCategory;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Offer offer = (Offer) o;
+        return Double.compare(offer.price, price) == 0
+                && Objects.equals(offerType, offer.offerType)
+                && Objects.equals(propertyCategory, offer.propertyCategory)
+                && Objects.equals(description, offer.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(offerType, propertyCategory, description, price);
+    }
+}
---
+++ team_real_estate/src/main/java/User.java
@@ -0,0 +1,62 @@
+import java.util.Objects;
+
+class User {
+    private String username;
+    private String password;
+    private String email;
+    private int id;
+
+    public User(String username, String password, String email, int id) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(username, user.username)
+                && Objects.equals(password, user.password)
+                && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password, email, id);
+    }
+}
