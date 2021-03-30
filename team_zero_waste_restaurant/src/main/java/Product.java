de46bc55a5809386836c3d04f9d06b69108a1984
Darxney <Tresutinsandris@gmail.com>
2021-03-30 17:46:39 +0300

    Create getter/setters, hashcode and toString for Product class

--- team_zero_waste_restaurant/src/main/java/Product.java
+++ team_zero_waste_restaurant/src/main/java/Product.java
@@ -1,8 +1,60 @@
 import java.util.Date;
+import java.util.Objects;
 
 public class Product {
     private String name;
     private int quantity;
     private Date expiryDate;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        return quantity == product.quantity && name.equals(product.name) && expiryDate.equals(product.expiryDate);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash;
+        return hash = 8 * Objects.hash(name, quantity, expiryDate) + 15;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", expiryDate=" + expiryDate +
+                '}';
+    }
+
 }
+
+
