dc97da660a6a326c3ee351f84b8cd70227cf65f9
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-04-04 18:03:50 +0300

    add database

---
+++ team_hr_system/src/main/java/hr_system/database/Database.java
@@ -0,0 +1,15 @@
+package hr_system.database;
+
+import hr_system.bean.Employee;
+
+import java.util.List;
+
+public interface Database {
+
+    void saveEmployee(Employee employee);
+
+    void deleteEmployee(Long id);
+
+    List <Employee> getAllEmployee() ;
+
+}
---
+++ team_hr_system/src/main/java/hr_system/database/DatabaseImpl.java
@@ -0,0 +1,37 @@
+package hr_system.database;
+
+import hr_system.bean.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseImpl implements Database{
+
+    private Long nextIdEmployee = 1L;
+    private List <Employee> employeeList = new ArrayList<>();
+
+    @Override
+    public void saveEmployee(Employee employee) {
+
+        employee.setId(nextIdEmployee);
+        nextIdEmployee++;
+        employeeList.add(employee);
+
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+
+        employeeList.stream().filter(employee -> employee.getId().equals(id))
+                .findFirst().
+                ifPresent(employeeList::remove);
+
+    }
+
+    @Override
+    public List<Employee> getAllEmployee() {
+
+        return employeeList;
+
+    }
+}
