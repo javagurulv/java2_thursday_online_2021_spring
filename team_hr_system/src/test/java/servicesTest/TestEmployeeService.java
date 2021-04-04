71eaf2aba97949b0c6756a58f64f277407a58e77
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-04-04 18:05:07 +0300

    add employee service test

---
+++ team_hr_system/src/test/java/servicesTest/TestEmployeeService.java
@@ -0,0 +1,81 @@
+package servicesTest;
+
+import hr_system.bean.Employee;
+import hr_system.database.DatabaseImpl;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestEmployeeService {
+
+    @Test
+    public void AddEmployeeServiceTest() {
+
+        DatabaseImpl database = new DatabaseImpl();
+
+        List<Employee> actual = new ArrayList<>();
+
+        Employee objectOne = new Employee("A1", "F1");
+        Employee objectTwo = new Employee("A2", "F2");
+
+        database.saveEmployee(objectOne);
+        database.saveEmployee(objectTwo);
+
+        actual.add(database.getAllEmployee().get(0));
+        actual.add(database.getAllEmployee().get(1));
+
+        List<Employee> expected = new ArrayList<>();
+
+        expected.add(objectOne);
+        expected.add(objectTwo);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void DeleteEmployeeServiceTest() {
+
+        DatabaseImpl database = new DatabaseImpl();
+
+        Employee objectOne = new Employee("A1", "F1");
+        Employee objectTwo = new Employee("A2", "F2");
+
+        database.saveEmployee(objectOne);
+        database.saveEmployee(objectTwo);
+
+        database.deleteEmployee(1L);
+
+        List<Employee> actual = new ArrayList<>(database.getAllEmployee());
+        List<Employee> expected = new ArrayList<>();
+
+        expected.add(objectTwo);
+
+        assertEquals(expected.get(0), actual.get(0));
+
+    }
+
+    @Test
+    public void GetAllEmployeeServiceTest() {
+
+        DatabaseImpl database = new DatabaseImpl();
+
+        Employee objectOne = new Employee("A1", "F1");
+        Employee objectTwo = new Employee("A2", "F2");
+
+        database.saveEmployee(objectOne);
+        database.saveEmployee(objectTwo);
+
+        List<Employee> actual = new ArrayList<>(database.getAllEmployee());
+        List<Employee> expected = new ArrayList<>();
+
+        expected.add(objectOne);
+        expected.add(objectTwo);
+
+        assertEquals(expected, actual);
+    }
+
+}
