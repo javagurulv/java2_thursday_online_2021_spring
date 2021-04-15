9a086f79448d82b3472cdcc1812b098adb10dcbc
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-04-15 15:46:38 +0300

    add registrationAdm and loginAdm. Implements this method

--- team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/database/Database.java
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/database/Database.java
@@ -1,11 +1,16 @@
 package lv.javaguru.java2.hrsystem.core.database;
 
+import lv.javaguru.java2.hrsystem.domain.Admin;
 import lv.javaguru.java2.hrsystem.domain.Employee;
 import lv.javaguru.java2.hrsystem.domain.EmployeeTitle;
 
 import java.util.List;
 
 public interface Database {
+
+    void registrationAdm (Admin admin);
+
+    boolean loginAdm(String email, String password);
 
     void saveEmployee(Employee employee);
 
--- team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/database/DatabaseImpl.java
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/database/DatabaseImpl.java
@@ -1,8 +1,13 @@
 package lv.javaguru.java2.hrsystem.core.database;
 
+import lv.javaguru.java2.hrsystem.domain.Admin;
 import lv.javaguru.java2.hrsystem.domain.Employee;
 import lv.javaguru.java2.hrsystem.domain.EmployeeTitle;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +16,37 @@ import static java.util.stream.Collectors.*;
 public class DatabaseImpl implements Database{
 
     private Long nextIdEmployee = 1L;
+    private Long nextIdAdmin = 1L;
     private List <Employee> employeeList = new ArrayList<>();
     private List<EmployeeTitle> employeeTitles = new ArrayList<>();
+    private static List<Admin> adminList = new ArrayList<>();
+    private static final String filename = "admin.txt";
+
+    static {
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(filename))) {
+            adminList = (List<Admin>) objectInputStream.readObject();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void registrationAdm(Admin admin) {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(filename))) {
+            admin.setId(nextIdAdmin);
+            nextIdAdmin++;
+            adminList.add(admin);
+            objectOutputStream.writeObject(admin);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public boolean loginAdm(String email, String password) {
+        return adminList.stream().anyMatch(admin -> admin.getEmail().equals(email)) &&
+                adminList.stream().anyMatch(admin -> admin.getPassword().equals(password));
+    }
 
     @Override
     public void saveEmployee(Employee employee) {
