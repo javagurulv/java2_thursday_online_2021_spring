69d2dc4e99ae4f727703ed332ee57858f0d08c08
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-04-21 22:30:58 +0300

    add databaseImplTest (refactoring from TestEmployeeService)

---
+++ team_hr_system/src/test/java/lv/javaguru/java2/hrsystem/core/databaseImpl/AddEmployeeImplTest.java
@@ -0,0 +1,38 @@
+package lv.javaguru.java2.hrsystem.core.databaseImpl;
+
+import lv.javaguru.java2.hrsystem.core.database.DatabaseImpl;
+import lv.javaguru.java2.hrsystem.domain.Employee;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class AddEmployeeImplTest {
+
+    @Test
+    public void addEmployeeServiceTest() {
+
+        DatabaseImpl database = new DatabaseImpl();
+
+        List<Employee> actual = new ArrayList<>();
+
+        Employee objectOne = new Employee("A1", "F1");
+        Employee objectTwo = new Employee("A2", "F2");
+
+        database.saveEmployee(objectOne);
+        database.saveEmployee(objectTwo);
+
+        actual.add(database.getAllEmployees().get(0));
+        actual.add(database.getAllEmployees().get(1));
+
+        List<Employee> expected = new ArrayList<>();
+
+        expected.add(objectOne);
+        expected.add(objectTwo);
+
+        assertEquals(expected, actual);
+
+    }
+}
---
+++ team_hr_system/src/test/java/lv/javaguru/java2/hrsystem/core/databaseImpl/GetEmployeesByTitleImplTest.java
@@ -0,0 +1,34 @@
+package lv.javaguru.java2.hrsystem.core.databaseImpl;
+
+import lv.javaguru.java2.hrsystem.core.database.DatabaseImpl;
+import lv.javaguru.java2.hrsystem.domain.Employee;
+import lv.javaguru.java2.hrsystem.domain.EmployeeTitle;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class GetEmployeesByTitleImplTest {
+
+    @Test
+    public void getEmployeesByTitleTest() {
+
+        DatabaseImpl database = new DatabaseImpl();
+
+        Employee objectOne = new Employee("A1", "F1", 21, EmployeeTitle.HR_MANAGER);
+        Employee objectTwo = new Employee("A2", "F1", 32, EmployeeTitle.DEVELOPER);
+
+        database.saveEmployee(objectOne);
+        database.saveEmployee(objectTwo);
+
+        List<Employee> actual = new ArrayList<>();
+        actual.add(objectTwo);
+
+        List<Employee> expected = new ArrayList<>(database.getEmployeesByTitle(EmployeeTitle.DEVELOPER));
+
+        assertEquals(expected, actual);
+
+    }
+}
