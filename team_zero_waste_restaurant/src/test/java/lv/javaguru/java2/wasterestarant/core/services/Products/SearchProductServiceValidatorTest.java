ab85270b3bb3b2f18a13399fc05e3d33c52625a4
Andrejs Breidaks <andrejs.breidaks@inbox.lv>
2021-04-25 20:26:51 +0300

    add SearchProductServiceTest (jUnit), add mock to the build

--- team_zero_waste_restaurant/src/test/java/lv/javaguru/java2/wasterestarant/core/services/Products/SearchProductServiceValidatorTest.java
+++ team_zero_waste_restaurant/src/test/java/lv/javaguru/java2/wasterestarant/core/services/Products/SearchProductServiceValidatorTest.java
@@ -1,4 +1,4 @@
-package lv.javaguru.java2.wasterestarant.core.services;
+package lv.javaguru.java2.wasterestarant.core.services.Products;
 
 import lv.javaguru.java2.wasterestarant.core.requests.Ordering;
 import lv.javaguru.java2.wasterestarant.core.requests.Paging;
