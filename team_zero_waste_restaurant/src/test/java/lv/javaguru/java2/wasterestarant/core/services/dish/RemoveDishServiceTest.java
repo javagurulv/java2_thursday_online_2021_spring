810c0d0f9acf2647da4c60adf55d6cd91efe4f5f
Andrejs Breidaks <andrejs.breidaks@inbox.lv>
2021-04-26 15:57:51 +0300

    add RemoveDishServiceTest, RemoveDishValidatorTest, package validators

---
+++ team_zero_waste_restaurant/src/test/java/lv/javaguru/java2/wasterestarant/core/services/dish/RemoveDishServiceTest.java
@@ -0,0 +1,52 @@
+package lv.javaguru.java2.wasterestarant.core.services.dish;
+
+import lv.javaguru.java2.wasterestarant.core.database.Database;
+import lv.javaguru.java2.wasterestarant.core.requests.dish.RemoveDishRequest;
+import lv.javaguru.java2.wasterestarant.core.responses.CoreError;
+import lv.javaguru.java2.wasterestarant.core.responses.dish.RemoveDishResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+
+// AndrejsB
+@RunWith(MockitoJUnitRunner.class)
+public class RemoveDishServiceTest {
+
+    @Mock private Database database;
+    @Mock private RemoveDishValidator validator;
+    @InjectMocks private RemoveDishService service;
+
+    @Test
+    public void shouldDeleteProductByName() {
+        Mockito.when(validator.validate(any())).thenReturn(new ArrayList<>());
+        Mockito.when(database.deleteDishByName("name")).thenReturn(true);
+        RemoveDishRequest request = new RemoveDishRequest("name");
+        RemoveDishResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        assertTrue(response.isDishNameDeleted());
+    }
+
+    @Test
+    public void shouldReturnErrorWhenDishNameIsEmpty() {
+        RemoveDishRequest request = new RemoveDishRequest(null);
+        List<CoreError> errors = new ArrayList<>();
+        errors.add(new CoreError("nameToRemove", "Must not be empty"));
+        Mockito.when(validator.validate(request)).thenReturn(errors);
+        RemoveDishResponse response = service.execute(request);
+        assertTrue(response.hasErrors());
+        assertEquals(response.getErrors().size(), 1);
+        assertEquals(response.getErrors().get(0).getField(), "nameToRemove");
+        assertEquals(response.getErrors().get(0).getMessage(), "Must not be empty");
+
+    }
+
+}
---
+++ team_zero_waste_restaurant/src/test/java/lv/javaguru/java2/wasterestarant/core/services/dish/RemoveDishValidatorTest.java
@@ -0,0 +1,31 @@
+package lv.javaguru.java2.wasterestarant.core.services.dish;
+
+
+import lv.javaguru.java2.wasterestarant.core.requests.dish.RemoveDishRequest;
+import lv.javaguru.java2.wasterestarant.core.responses.CoreError;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+// AndrejsB
+public class RemoveDishValidatorTest {
+
+    private RemoveDishValidator validator = new RemoveDishValidator();
+
+    @Test
+    public void shouldReturnErrorWhenNameIsNull() {
+        RemoveDishRequest request = new RemoveDishRequest(null);
+        List<CoreError> errors = validator.validate(request);
+        assertEquals(errors.size(), 1);
+        assertEquals(errors.get(0).getField(), "nameToRemove");
+        assertEquals(errors.get(0).getMessage(), "Must not be empty");
+    }
+
+    @Test
+    public void shouldRemoveProduct() {
+        RemoveDishRequest request = new RemoveDishRequest("name");
+        List<CoreError> errors = validator.validate(request);
+        assertEquals(errors.size(), 0);
+    }
+}
