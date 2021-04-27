20c7e583bef76e68ad9eba2c94035e1ac2b83ee1
Viktor <doc18@inbox.lv>
2021-04-27 21:32:26 +0300

    Tests added

---
+++ team_real_estate/src/test/java/lv/javaguru/java2/realestate/core/services/CreateOfferServiceTest.java
@@ -0,0 +1,59 @@
+package lv.javaguru.java2.realestate.core.services;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lv.javaguru.java2.realestate.matchers.OfferMatcher;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import lv.javaguru.java2.realestate.core.database.Database;
+import lv.javaguru.java2.realestate.core.requests.CreateOfferRequest;
+import lv.javaguru.java2.realestate.core.response.CoreError;
+import lv.javaguru.java2.realestate.core.response.CreateOfferResponse;
+import lv.javaguru.java2.realestate.core.services.validators.CreateOfferValidator;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CreateOfferServiceTest {
+
+    @Mock
+    private Database database;
+    @Mock
+    private CreateOfferValidator validator;
+    @InjectMocks
+    private CreateOfferService service;
+
+    @Test
+    public void shouldReturnResponseWithErrorsWhenValidationFails() {
+        CreateOfferRequest request = new CreateOfferRequest(null, "Category", "Description", 20.0);
+        List<CoreError> errors = new ArrayList<>();
+        errors.add(new CoreError("offerType", "Must not be empty !"));
+        Mockito.when(validator.validate(request)).thenReturn(errors);
+
+        CreateOfferResponse response = service.execute(request);
+        assertTrue(response.hasErrors());
+        assertEquals(response.getErrors().size(), 1);
+        assertEquals(response.getErrors().get(0).getField(), "offerType");
+        assertEquals(response.getErrors().get(0).getMessage(), "Must not be empty !");
+
+        Mockito.verifyNoInteractions(database);
+    }
+
+    @Test
+    public void shouldCreateOfferInDatabase() {
+        Mockito.when(validator.validate(any())).thenReturn(new ArrayList<>());
+        CreateOfferRequest request = new CreateOfferRequest("Type", "Category", "Description", 20.0);
+        CreateOfferResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        Mockito.verify(database).createOffer(
+                argThat(new OfferMatcher("Type", "Category", "Description", 20.0)));
+    }
+}
