a4707f4f2aa7977dadf33e6c81380bca372ef05e
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-04-27 18:53:57 +0300

    renaming, add field userRole

--- team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/requests/RegistrationRequest.java
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/requests/RegistrationRequest.java
@@ -1,17 +1,25 @@
 package lv.javaguru.java2.hrsystem.core.requests;
 
+import lv.javaguru.java2.hrsystem.domain.UserRole;
+
 public class RegistrationRequest {
 
+    private UserRole userRole;
     private String firstName;
     private String secondName;
     private String email;
     private String password;
 
-    public RegistrationRequest(String firstName, String secondName, String email, String password) {
+    public RegistrationRequest(UserRole userRole, String firstName, String secondName, String email, String password) {
+        this.userRole = userRole;
         this.firstName = firstName;
         this.secondName = secondName;
         this.email = email;
         this.password = password;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
     }
 
     public String getFirstName() {
