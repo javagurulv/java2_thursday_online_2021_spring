b6158d07cb23b031d153a670a7f25c1f0dd23809
j-sce <j.scerbicka@gmail.com>
2021-04-27 17:21:19 +0300

    ordering added

--- team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/requests/SearchEmployeesRequest.java
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/requests/SearchEmployeesRequest.java
@@ -4,6 +4,8 @@ public class SearchEmployeesRequest {
     private final String employeeTitle;
     private final String name;
 
+    private Ordering ordering;
+
     private Paging paging;
 
     public SearchEmployeesRequest(String employeeTitle, String name) {
@@ -11,9 +13,22 @@ public class SearchEmployeesRequest {
         this.name = name;
     }
 
+    public SearchEmployeesRequest(String employeeTitle, String name, Ordering ordering) {
+        this.employeeTitle = employeeTitle;
+        this.name = name;
+        this.ordering = ordering;
+    }
+
     public SearchEmployeesRequest(String employeeTitle, String name, Paging paging) {
         this.employeeTitle = employeeTitle;
         this.name = name;
+        this.paging = paging;
+    }
+
+    public SearchEmployeesRequest(String employeeTitle, String name, Ordering ordering, Paging paging) {
+        this.employeeTitle = employeeTitle;
+        this.name = name;
+        this.ordering = ordering;
         this.paging = paging;
     }
 
@@ -23,6 +38,10 @@ public class SearchEmployeesRequest {
 
     public String getEmployeeTitle() {
         return employeeTitle;
+    }
+
+    public Ordering getOrdering() {
+        return ordering;
     }
 
     public Paging getPaging() {
--- team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/services/validators/SearchEmployeesRequestValidator.java
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/services/validators/SearchEmployeesRequestValidator.java
@@ -1,5 +1,6 @@
 package lv.javaguru.java2.hrsystem.core.services.validators;
 
+import lv.javaguru.java2.hrsystem.core.requests.Ordering;
 import lv.javaguru.java2.hrsystem.core.requests.Paging;
 import lv.javaguru.java2.hrsystem.core.requests.SearchEmployeesRequest;
 import lv.javaguru.java2.hrsystem.core.responses.CoreError;
@@ -14,8 +15,14 @@ public class SearchEmployeesRequestValidator {
 
     public List<CoreError> validate(SearchEmployeesRequest request) {
         List<CoreError> errors = new ArrayList<>();
-
         errors.addAll(validateSearchFields(request));
+
+        if(request.getOrdering() != null){
+            validateOrderBy(request.getOrdering()).ifPresent(errors::add);
+            validateOrderDirection(request.getOrdering()).ifPresent(errors::add);
+            validateMandatoryOrderBy(request.getOrdering()).ifPresent(errors::add);
+            validateMandatoryOrderDirection(request.getOrdering()).ifPresent(errors::add);
+        }
 
         if (request.getPaging() != null) {
             validatePageNumber(request.getPaging()).ifPresent(errors::add);
@@ -63,6 +70,33 @@ public class SearchEmployeesRequestValidator {
         }
         return valid;
     }
+
+    private Optional<CoreError> validateOrderBy(Ordering ordering) {
+        return (ordering.getOrderBy() != null
+                && !(ordering.getOrderBy().equals("name") || ordering.getOrderBy().equals("title")))
+                ? Optional.of(new CoreError("orderBy", "Must contain 'name' or 'title' only!"))
+                : Optional.empty();
+    }
+
+    private Optional<CoreError> validateOrderDirection(Ordering ordering) {
+        return (ordering.getOrderDirection() != null
+                && !(ordering.getOrderDirection().equals("ASCENDING") || ordering.getOrderDirection().equals("DESCENDING")))
+                ? Optional.of(new CoreError("orderDirection", "Must contain 'ASCENDING' or 'DESCENDING' only!"))
+                : Optional.empty();
+    }
+
+    private Optional<CoreError> validateMandatoryOrderBy(Ordering ordering) {
+        return (ordering.getOrderDirection() != null && ordering.getOrderBy() == null)
+                ? Optional.of(new CoreError("orderBy", "Must not be empty!"))
+                : Optional.empty();
+    }
+
+    private Optional<CoreError> validateMandatoryOrderDirection(Ordering ordering) {
+        return (ordering.getOrderBy() != null && ordering.getOrderDirection() == null)
+                ? Optional.of(new CoreError("orderDirection", "Must not be empty!"))
+                : Optional.empty();
+    }
+
 
     private Optional<CoreError> validatePageNumber(Paging paging) {
         return (paging.getPageNumber() != null
