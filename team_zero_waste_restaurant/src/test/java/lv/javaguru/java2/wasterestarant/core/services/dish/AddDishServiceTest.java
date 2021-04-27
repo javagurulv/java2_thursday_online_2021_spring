9cadff3b45b8f78ce8011731baa0d1ccf5a386e7
Darxney <Tresutinsandris@gmail.com>
2021-04-27 18:57:45 +0300

    Add AddDishService and AddDishValidator tests

---
+++ team_zero_waste_restaurant/src/test/java/lv/javaguru/java2/wasterestarant/core/services/dish/AddDishServiceTest.java
@@ -0,0 +1,57 @@
+package lv.javaguru.java2.wasterestarant.core.services.dish;
+
+import lv.javaguru.java2.wasterestarant.core.database.Database;
+import lv.javaguru.java2.wasterestarant.core.requests.dish.AddDishRequest;
+import lv.javaguru.java2.wasterestarant.core.responses.CoreError;
+import lv.javaguru.java2.wasterestarant.core.responses.dish.AddDishResponse;
+import matchers.DishMatcher;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+
+@RunWith(MockitoJUnitRunner.class)
+//Andris
+public class AddDishServiceTest {
+
+    @Mock
+    private Database database;
+    @Mock
+    private AddDishValidator validator;
+    @InjectMocks
+    private AddDishService service;
+
+    @Test
+    public void shouldAddDishToDatabase()  {
+        Mockito.when(validator.validate(any())).thenReturn(new ArrayList<>());
+        AddDishRequest request = new AddDishRequest("name", "desc","soup", 1.0, 2.0);
+        AddDishResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        Mockito.verify(database).save(
+                argThat(new DishMatcher("name","desc","soup", 1.0, 2.0)));
+    }
+
+    @Test
+    public void shouldReturnResponseWithErrorsWhenValidationFails() {
+        AddDishRequest request = new AddDishRequest(null,"desc","soup", 1.0, 2.0);
+        List<CoreError> errors = new ArrayList<>();
+        errors.add(new CoreError("name", "Must not be empty!"));
+        Mockito.when(validator.validate(request)).thenReturn(errors);
+
+        AddDishResponse response = service.execute(request);
+        assertTrue(response.hasErrors());
+        assertEquals(response.getErrors().size(), 1);
+        assertEquals(response.getErrors().get(0).getField(), "name");
+        assertEquals(response.getErrors().get(0).getMessage(), "Must not be empty!");
+
+        Mockito.verifyNoInteractions(database);
+    }
+
+}
