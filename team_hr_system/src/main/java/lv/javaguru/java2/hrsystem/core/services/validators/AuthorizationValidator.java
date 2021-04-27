95aa75cf4f1b8b3298c2454f39a93f2fbdc182ea
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-04-27 18:58:18 +0300

    refactoring

--- team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/services/validators/AuthorizationValidator.java
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/services/validators/AuthorizationValidator.java
@@ -1,13 +1,13 @@
 package lv.javaguru.java2.hrsystem.core.services.validators;
 
-import lv.javaguru.java2.hrsystem.core.requests.LoginRequest;
+import lv.javaguru.java2.hrsystem.core.requests.AuthorizationRequest;
 import lv.javaguru.java2.hrsystem.core.responses.CoreError;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class LoginValidator {
-    public List<CoreError> validate (LoginRequest request) {
+public class AuthorizationValidator {
+    public List<CoreError> validate (AuthorizationRequest request) {
         List<CoreError> errors = new ArrayList<>();
 
         validateEmail(request).ifPresent(errors::add);
@@ -16,27 +16,20 @@ public class LoginValidator {
         return errors;
     }
 
-    private Optional<CoreError> validateEmail(LoginRequest request) {
+    private Optional<CoreError> validateEmail(AuthorizationRequest request) {
         if (request.getEmail() == null || request.getEmail().isEmpty()) {
-
             return Optional.of(new CoreError("email", "Must not be empty!"));
-
         } else {
-
             return Optional.empty();
 
         }
     }
 
-    private Optional<CoreError> validatePassword(LoginRequest request) {
+    private Optional<CoreError> validatePassword(AuthorizationRequest request) {
         if (request.getPassword() == null || request.getPassword().isEmpty()){
-
             return Optional.of(new CoreError("password", "Must not be empty!"));
-
         } else {
-
             return Optional.empty();
-
         }
     }
 }
