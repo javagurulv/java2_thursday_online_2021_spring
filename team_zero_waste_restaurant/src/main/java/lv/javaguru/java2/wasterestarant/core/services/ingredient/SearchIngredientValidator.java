612e22f5187ba89a6da61d1c7630ac3aa6b3d5e2
Darxney <Tresutinsandris@gmail.com>
2021-04-28 17:03:19 +0300

    SearchIngredientValidatorTest

--- team_zero_waste_restaurant/src/main/java/lv/javaguru/java2/wasterestarant/core/services/ingredient/SearchIngredientValidator.java
+++ team_zero_waste_restaurant/src/main/java/lv/javaguru/java2/wasterestarant/core/services/ingredient/SearchIngredientValidator.java
@@ -1,16 +1,31 @@
 package lv.javaguru.java2.wasterestarant.core.services.ingredient;
 
+import lv.javaguru.java2.wasterestarant.core.requests.Ordering;
+import lv.javaguru.java2.wasterestarant.core.requests.Paging;
 import lv.javaguru.java2.wasterestarant.core.requests.ingredient.SearchIngredientRequest;
 import lv.javaguru.java2.wasterestarant.core.responses.CoreError;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class SearchIngredientValidator {
 
     public List<CoreError> validate(SearchIngredientRequest request) {
         List<CoreError> errors = new ArrayList<>();
         errors.addAll(validateSearchFields(request));
+        if (request.getOrdering() != null) {
+            validateOrderBy(request.getOrdering()).ifPresent(errors::add);
+            validateOrderDirection(request.getOrdering()).ifPresent(errors::add);
+            validateMandatoryOrderBy(request.getOrdering()).ifPresent(errors::add);
+            validateMandatoryOrderDirection(request.getOrdering()).ifPresent(errors::add);
+        }
+        if (request.getPaging() != null) {
+            validatePageNumber(request.getPaging()).ifPresent(errors::add);
+            validatePageSize(request.getPaging()).ifPresent(errors::add);
+            validateMandatoryPageNumber(request.getPaging()).ifPresent(errors::add);
+            validateMandatoryPageSize(request.getPaging()).ifPresent(errors::add);
+        }
         return errors;
     }
 
@@ -24,6 +39,58 @@ public class SearchIngredientValidator {
 
     private boolean isEmpty(String str) {
         return str == null || str.isEmpty();
+    }
+
+    private Optional<CoreError> validateOrderBy(Ordering ordering) {
+        return (ordering.getOrderBy() != null
+                && !(ordering.getOrderBy().equals("N") || ordering.getOrderBy().equals("Q")))
+                ? Optional.of(new CoreError("orderBy", "Must contain 'N' or 'Q' only"))
+                : Optional.empty();
+    }
+
+    private Optional<CoreError> validateOrderDirection(Ordering ordering) {
+        return (ordering.getOrderDirection() != null
+                && !(ordering.getOrderDirection().equals("A") || ordering.getOrderDirection().equals("D")))
+                ? Optional.of(new CoreError("orderDirection", "Must contain 'A' or 'D' only"))
+                : Optional.empty();
+    }
+
+    private Optional<CoreError> validateMandatoryOrderBy(Ordering ordering) {
+        return (ordering.getOrderDirection() != null && ordering.getOrderBy() == null)
+                ? Optional.of(new CoreError("orderBy", "Must not be empty"))
+                : Optional.empty();
+    }
+
+    private Optional<CoreError> validateMandatoryOrderDirection(Ordering ordering) {
+        return (ordering.getOrderBy() != null && ordering.getOrderDirection() == null)
+                ? Optional.of(new CoreError("orderDirection", "Must not be empty"))
+                : Optional.empty();
+    }
+
+    private Optional<CoreError> validatePageNumber(Paging paging) {
+        return (paging.getPageNumber() != null
+                && paging.getPageNumber() <= 0)
+                ? Optional.of(new CoreError("pageNumber", "Must be greater than 0"))
+                : Optional.empty();
+    }
+
+    private Optional<CoreError> validatePageSize(Paging paging) {
+        return (paging.getPageSize() != null
+                && paging.getPageSize() <= 0)
+                ? Optional.of(new CoreError("pageSize", "Must be greater than 0"))
+                : Optional.empty();
+    }
+
+    private Optional<CoreError> validateMandatoryPageNumber(Paging paging) {
+        return (paging.getPageNumber() == null && paging.getPageSize() != null)
+                ? Optional.of(new CoreError("pageNumber", "Must not be empty"))
+                : Optional.empty();
+    }
+
+    private Optional<CoreError> validateMandatoryPageSize(Paging paging) {
+        return (paging.getPageSize() == null && paging.getPageNumber() != null)
+                ? Optional.of(new CoreError("pageSize", "Must not be empty"))
+                : Optional.empty();
     }
 
 
