c8bbe3f5d4a6bcc7a23851983ed16e4ec1d52f47
Aleksandrs Bulahs <aleksandrs.bulahs@gmail.com>
2021-05-03 23:08:57 +0300

    Unit test addLot , Login

--- team_front_of_engineers_hardcore/src/test/java/lv/javaguru/java2/hardcore/services/lot/AddLotServiceTest.java
+++ team_front_of_engineers_hardcore/src/test/java/lv/javaguru/java2/hardcore/services/lot/AddLotServiceTest.java
@@ -2,6 +2,7 @@ package lv.javaguru.java2.hardcore.services.lot;
 
 import lv.javaguru.java2.hardcore.database.LotDatabase;
 import lv.javaguru.java2.hardcore.database.UserDatabase;
+import lv.javaguru.java2.hardcore.domain.Lot;
 import lv.javaguru.java2.hardcore.domain.User;
 import lv.javaguru.java2.hardcore.requests.lot.AddLotRequest;
 import lv.javaguru.java2.hardcore.response.CoreError;
@@ -10,40 +11,50 @@ import lv.javaguru.java2.hardcore.services.user.LoginService;
 import lv.javaguru.java2.hardcore.services.user.LoginValidator;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
-/*
+@RunWith(MockitoJUnitRunner.class)
 public class AddLotServiceTest {
 
-    private LotDatabase lotDatabase;
-    private AddLotValidator lotValidator;
-    //private AddLotService service;
-    private LoginService loginService;
+    @Mock private LotDatabase lotDatabase;
+    @Mock private Lot lot;
+    @Mock private AddLotValidator lotValidator;
+    @Mock private LoginService loginService;
+    @InjectMocks
+    private AddLotService service;
 
-
-    @Before
-    public void init() {
-        lotDatabase = Mockito.mock(LotDatabase.class);
-        lotValidator = Mockito.mock(AddLotValidator.class);
-       // loginService = Mockito.mock(LoginService.class);
-        //service = new AddLotService(lotDatabase,lotValidator, loginService);
-
-
-    }
 
     @Test
-    public void shoudReturntValidatorErrors() {
+    public void shouldReturntValidatorErrors() {
+        List<CoreError> errors = new ArrayList<>();
         CoreError error = new CoreError("test1","test1");
-        AddLotRequest request = new AddLotRequest(null, null);
+        errors.add(error);
+        AddLotRequest request = new AddLotRequest("abc", BigDecimal.ONE);
 
-        Mockito.when(lotValidator.validate(request)).thenReturn(List.of(error))
+        Mockito.when(lotValidator.validate(request)).thenReturn(errors);
 
         AddLotResponse response = service.execute(request);
 
-        //assertTrue(response.hasErrors());
+        assertTrue(response.hasErrors());
 
+        assertEquals(response.getErrors().size(), 1);
     }
-}*/
+
+    @Test
+    public void shouldReturnCorrectResponce() {
+        AddLotRequest request = new AddLotRequest("abc", BigDecimal.ONE);
+        Mockito.when(lotValidator.validate(request)).thenReturn(List.of());
+        AddLotResponse response = service.execute(request);
+        Lot lot = response.getNewLot();
+        Mockito.verify(lotDatabase).saveLot(lot);
+    }
+}
