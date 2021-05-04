990a7e38436a6932a39642c188799b62a012c12a
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-05-04 19:28:39 +0300

    add AcceptanceTestAuthorizeUser

---
+++ team_hr_system/src/test/java/lv/javaguru/java2/hrsystem/acceptancetests/AcceptanceTestAuthorizeUser.java
@@ -0,0 +1,48 @@
+package lv.javaguru.java2.hrsystem.acceptancetests;
+
+import lv.javaguru.java2.hrsystem.application.ApplicationContext;
+import lv.javaguru.java2.hrsystem.core.requests.*;
+import lv.javaguru.java2.hrsystem.core.responses.GetAllUsersResponse;
+import lv.javaguru.java2.hrsystem.core.services.*;
+import lv.javaguru.java2.hrsystem.domain.*;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AcceptanceTestAuthorizeUser {
+
+    private ApplicationContext context = new ApplicationContext();
+
+    @Test
+    public void shouldReturnAuthorizeUser() {
+        RegisterUserRequest registerUserRequest = new RegisterUserRequest(UserRole.ADMIN,
+                "null", "null", "null@null.null", "null123456");
+        AuthorizeUserRequest authorizeUserRequest = new AuthorizeUserRequest(registerUserRequest.getEmail(),
+                registerUserRequest.getPassword());
+
+        registerUserService().execute(registerUserRequest);
+        authorizeUserService().execute(authorizeUserRequest);
+
+        GetAllUsersResponse getAllUsersResponse = getAllUsersService().execute();
+
+        assertThat(getAllUsersResponse.getUsers().size() >= 1).isTrue();
+
+        User expectedUser = new User(UserRole.ADMIN,
+                "null", "null", "null@null.null", "null123456");
+        expectedUser.setId(1L);
+
+        assertThat(getAllUsersResponse.getUsers().get(0)).isEqualTo(expectedUser);
+    }
+
+    private RegisterUserService registerUserService() {
+        return context.getBean(RegisterUserService.class);
+    }
+
+    private AuthorizeUserService authorizeUserService() {
+        return context.getBean(AuthorizeUserService.class);
+    }
+
+    private GetAllUsersService getAllUsersService() {
+        return context.getBean(GetAllUsersService.class);
+    }
+}
