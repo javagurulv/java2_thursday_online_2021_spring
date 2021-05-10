ebcf6d08f79a4ee6dd66d48c5b82cfb653a371ce
Jelena Lavrenuka <lena.lavrenuka@gmail.com>
2021-05-10 21:30:21 +0300

    app update

--- team_zero_waste_restaurant/src/main/java/lv/javaguru/java2/wasterestarant/RestaurantApplication.java
+++ team_zero_waste_restaurant/src/main/java/lv/javaguru/java2/wasterestarant/RestaurantApplication.java
@@ -2,22 +2,8 @@ package lv.javaguru.java2.wasterestarant;
 
 import lv.javaguru.java2.wasterestarant.config.RestaurantApplicationConfiguration;
 import lv.javaguru.java2.wasterestarant.console_ui.*;
-import lv.javaguru.java2.wasterestarant.console_ui.dish.AddDishUIAction;
-import lv.javaguru.java2.wasterestarant.console_ui.dish.GetAllDishesUIAction;
-import lv.javaguru.java2.wasterestarant.console_ui.dish.RemoveDishUIAction;
-import lv.javaguru.java2.wasterestarant.console_ui.dish.SearchDishesUIAction;
-import lv.javaguru.java2.wasterestarant.console_ui.ingredient.AddDishIngredientUiAction;
-import lv.javaguru.java2.wasterestarant.console_ui.ingredient.SearchIngredientUiAction;
-import lv.javaguru.java2.wasterestarant.console_ui.order.GetAllOrdersUIAction;
-import lv.javaguru.java2.wasterestarant.console_ui.product.AddProductUIAction;
-import lv.javaguru.java2.wasterestarant.console_ui.product.GetAllProductsUIAction;
-import lv.javaguru.java2.wasterestarant.console_ui.product.SearchProductUIAction;
-import lv.javaguru.java2.wasterestarant.console_ui.wishlist.AddDishToWishlistUIAction;
-import lv.javaguru.java2.wasterestarant.console_ui.wishlist.GetWishListUIAction;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.util.Scanner;
 
 public class RestaurantApplication {
 
