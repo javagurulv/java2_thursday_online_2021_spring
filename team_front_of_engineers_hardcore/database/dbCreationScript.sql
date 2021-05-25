e1b69eabe1a8a1eebea182f4f425566b41c81a62
Vladislav <vladislav34812@gmail.com>
2021-05-25 20:34:25 +0300

    Commit

--- team_front_of_engineers_hardcore/database/dbCreationScript.sql
+++ team_front_of_engineers_hardcore/database/dbCreationScript.sql
@@ -47,34 +47,6 @@ AUTO_INCREMENT = 1000;
   ADD FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`);
 
 
-insert into users(name, password)
-values ("nameOne", "passwordOne");
-insert into users(name, password)
-values ("nameTwo", "passwordTwo");
-insert into users(name, password)
-values ("nameThree", "passwordThree");
-
-
-
-insert into user_lots(name,start_price,user_id)
-values ("Bycicle",200,1000);
-insert into user_lots(name,start_price,user_id)
-values ("Book",1.29,1001);
-insert into user_lots(name,start_price,user_id)
-values ("Athlon2CPU",79,1001);
-insert into user_lots(name,start_price,user_id)
-values ("AuthenticLighter",50,1002);
-insert into user_lots(name,start_price,user_id)
-values ("GTX1080",99.99,1001);
-
-
-insert into user_bets(lot_id,bet,user_id)
-value(1000,250,1002);
-insert into user_bets(lot_id,bet,user_id)
-value(1000,270,1001);
-insert into user_bets(lot_id,bet,user_id)
-value(1004,120,1001);
-
 
 -- COMMANDS --
 SELECT * FROM users;
