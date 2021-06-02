521e099f994f6bceefd00e6ac4e6a427811a7a8c
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-06-02 21:54:44 +0300

    add scripts for TEST database

---
+++ team_hr_system/src/test/database_test/dbDataDeleteScript.sql
@@ -0,0 +1,20 @@
+DELETE FROM employee_skills
+WHERE id > 0;
+
+DELETE FROM skills
+WHERE id > 0;
+
+DELETE FROM employees
+WHERE id > 0;
+
+DELETE FROM employee_titles
+WHERE id > 0;
+
+DELETE FROM user_employees
+WHERE id > 0;
+
+DELETE FROM users_log
+WHERE user_id > 0;
+
+DELETE FROM users
+WHERE id > 0;
