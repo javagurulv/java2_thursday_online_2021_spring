e66d45f242e28cc77d2c15d2dc096049f3e3c9bd
jlavrenuka <Jelena.Lavrenuka@proofit.lv>
2021-06-07 10:54:10 +0300

    SQL scripts update

--- team_zero_waste_restaurant/database/dbDataUpdateScript.sql
+++ team_zero_waste_restaurant/database/dbDataUpdateScript.sql
@@ -49,3 +49,6 @@ DROP client_id;
 ALTER TABLE order_item
 ADD column order_list_id BIGINT       NOT NULL,
 ADD FOREIGN KEY (order_list_id) REFERENCES order_list (id);
+
+ALTER TABLE user_list
+ADD FOREIGN KEY (user_role) REFERENCES user_role (id);
