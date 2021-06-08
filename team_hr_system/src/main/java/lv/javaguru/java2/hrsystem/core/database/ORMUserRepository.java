a097d8c6bb11a3de20660f387909e6f1cae0300a
kaverski <jelena.kaverska@28stone.com>
2021-06-08 22:47:36 +0300

    added annotation

--- team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/database/ORMUserRepository.java
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/database/ORMUserRepository.java
@@ -2,11 +2,15 @@ package lv.javaguru.java2.hrsystem.core.database;
 
 import lv.javaguru.java2.hrsystem.core.domain.User;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-
+@Component
+@Transactional
 public class ORMUserRepository {
 
-    //@Autowired
+    @Autowired
     private SessionFactory sessionFactory;
 
     public void registerUser(User user) {
