513d32393bc91e15c990459bd06cdae5da634467
kaverski <jelena.kaverska@28stone.com>
2021-06-09 14:19:49 +0300

    added app properties to test

--- team_hr_system/src/test/java/lv/javaguru/java2/hrsystem/integrationtests/SpringContextTest.java
+++ team_hr_system/src/test/java/lv/javaguru/java2/hrsystem/integrationtests/SpringContextTest.java
@@ -1,6 +1,16 @@
 package lv.javaguru.java2.hrsystem.integrationtests;
 
-/*
+
+import lv.javaguru.java2.hrsystem.config.HRSystemConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertNotNull;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {HRSystemConfiguration.class})
 public class SpringContextTest {
@@ -13,4 +23,4 @@ public class SpringContextTest {
         assertNotNull(appContext);
     }
 
-}*/
+}
