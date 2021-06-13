6dfcc0a97413cf8153a046de39cd908322e8a484
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-06-13 19:39:23 +0300

    add DeleteUserUIAction and UpdateUserRoleUIAction

---
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/console_ui/DeleteUserUIAction.java
@@ -0,0 +1,64 @@
+package lv.javaguru.java2.hrsystem.console_ui;
+
+import lv.javaguru.java2.hrsystem.core.requests.DeleteUserRequest;
+import lv.javaguru.java2.hrsystem.core.responses.DeleteUserResponse;
+import lv.javaguru.java2.hrsystem.core.services.DeleteUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
+
+@Component
+public class DeleteUserUIAction implements UIAction{
+
+    @Autowired
+    private DeleteUserService deleteUserService;
+
+    @Override
+    public void execute() {
+        Long id = getId();
+
+        DeleteUserRequest deleteUserRequest = new DeleteUserRequest(id);
+        DeleteUserResponse deleteUserResponse = deleteUserService.execute(deleteUserRequest);
+
+        if (deleteUserResponse.hasErrors()) {
+
+            deleteUserResponse.getErrors().forEach(e -> System.out.println(e.getField() + " " + e.getMessage()));
+
+        } else if (deleteUserResponse.isRemoved()) {
+            System.out.println("User removed!");
+        } else {
+            System.out.println("User is not removed!");
+        }
+    }
+
+    private Long getId() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter user ID!");
+
+        boolean valid = false;
+
+        Long id = null;
+
+        while (!valid) {
+            try {
+
+                id = Long.parseLong(scanner.nextLine());
+                valid = true;
+
+            } catch (NumberFormatException e) {
+
+                System.out.println("Invalid input. Please enter numeric value!");
+                valid = false;
+
+            }
+        }
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Delete User";
+    }
+}
---
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/console_ui/UpdateUserRoleUIAction.java
@@ -0,0 +1,76 @@
+package lv.javaguru.java2.hrsystem.console_ui;
+
+import lv.javaguru.java2.hrsystem.core.domain.UserRole;
+import lv.javaguru.java2.hrsystem.core.requests.UpdateUserRoleRequest;
+import lv.javaguru.java2.hrsystem.core.responses.UpdateUserRoleResponse;
+import lv.javaguru.java2.hrsystem.core.services.UpdateUserRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
+public class UpdateUserRoleUIAction implements UIAction{
+
+    @Autowired UpdateUserRoleService updateUserRoleService;
+
+    private Map<Integer, UserRole> userRoleMap = new HashMap<>() {{
+        put(1, UserRole.ADMIN);
+        put(2, UserRole.HR_MANAGE);}};
+
+    @Override
+    public void execute() {
+        Long id = getId();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Select role: ");
+        userRoleMap.forEach((key, value) -> System.out.println(key + " - " + value));
+
+        int select = scanner.nextInt();
+
+        UpdateUserRoleRequest updateUserRoleRequest = new UpdateUserRoleRequest(id, userRoleMap.get(select));
+        UpdateUserRoleResponse updateUserRoleResponse = updateUserRoleService.execute(updateUserRoleRequest);
+
+        if(updateUserRoleResponse.getUpdate()){
+
+            System.out.println("Update completed!");
+
+        } else {
+
+            System.out.println("Error!");
+
+        }
+    }
+
+    private Long getId() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter user ID!");
+
+        boolean valid = false;
+
+        Long id = null;
+
+        while (!valid) {
+            try {
+
+                id = Long.parseLong(scanner.nextLine());
+                valid = true;
+
+            } catch (NumberFormatException e) {
+
+                System.out.println("Invalid input. Please enter numeric value!");
+                valid = false;
+
+            }
+        }
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Update User role";
+    }
+
+}
