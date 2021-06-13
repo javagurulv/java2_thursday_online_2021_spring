4a6a0b528b3e99312de099a2db9f227580ae6fc5
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-06-13 19:34:32 +0300

    add DeleteUserRequest and UpdateUserRoleRequest

---
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/requests/DeleteUserRequest.java
@@ -0,0 +1,17 @@
+package lv.javaguru.java2.hrsystem.core.requests;
+
+public class DeleteUserRequest {
+    private Long id;
+
+    public DeleteUserRequest(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/requests/UpdateUserRoleRequest.java
@@ -0,0 +1,29 @@
+package lv.javaguru.java2.hrsystem.core.requests;
+
+import lv.javaguru.java2.hrsystem.core.domain.UserRole;
+
+public class UpdateUserRoleRequest {
+    private Long id;
+    private UserRole userRole;
+
+    public UpdateUserRoleRequest(Long id, UserRole userRole) {
+        this.id = id;
+        this.userRole = userRole;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+}
