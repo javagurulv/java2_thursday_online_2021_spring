8b4e64169af6f017a50980e91deac0662afb33cd
sinnaty <nataliasin@gmail.com>
2021-06-27 12:49:37 +0300

    zero_waste_restaurant - get all orders controller and html

---
+++ team_zero_waste_restaurant/src/main/java/lv/javaguru/java2/wasterestarant/web_ui/controllers/order/GetAllOrdersController.java
@@ -0,0 +1,25 @@
+package lv.javaguru.java2.wasterestarant.web_ui.controllers.order;
+
+import lv.javaguru.java2.wasterestarant.core.requests.order.GetAllOrdersRequest;
+import lv.javaguru.java2.wasterestarant.core.responses.order.GetAllOrdersResponse;
+import lv.javaguru.java2.wasterestarant.core.services.order.GetAllOrdersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GetAllOrdersController {
+
+    @Autowired
+    private GetAllOrdersService getAllOrdersService;
+
+    @GetMapping(value = "/getAllOrders")
+    public String getAllDishes(ModelMap modelMap) {
+        GetAllOrdersResponse response = getAllOrdersService.execute(new GetAllOrdersRequest());
+
+        modelMap.addAttribute("orders", response.getOrders());
+        return "/getAllOrders";
+    }
+
+}
