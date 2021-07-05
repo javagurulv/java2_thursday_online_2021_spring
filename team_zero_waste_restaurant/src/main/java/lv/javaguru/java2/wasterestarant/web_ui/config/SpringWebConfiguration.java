170283b24ca1fac2aa8ce3281bf7a7512943fc87
Andrejs Breidaks <andrejs.breidaks@inbox.lv>
2021-07-05 15:37:22 +0300

    add 3 lines to webConfig

--- team_zero_waste_restaurant/src/main/java/lv/javaguru/java2/wasterestarant/web_ui/config/SpringWebConfiguration.java
+++ team_zero_waste_restaurant/src/main/java/lv/javaguru/java2/wasterestarant/web_ui/config/SpringWebConfiguration.java
@@ -1,9 +1,11 @@
 package lv.javaguru.java2.wasterestarant.web_ui.config;
 
 
+import lv.javaguru.java2.wasterestarant.config.StringToEnumConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -21,6 +23,7 @@ public class SpringWebConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/resources/**")
                 .addResourceLocations("/resources/");
     }
+
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver resourceViewResolver = new SpringResourceTemplateResolver();
@@ -30,5 +33,10 @@ public class SpringWebConfiguration implements WebMvcConfigurer {
         resourceViewResolver.setCharacterEncoding("UTF-8");
         resourceViewResolver.setCheckExistence(false);
         return resourceViewResolver;
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToEnumConverter());
     }
 }
