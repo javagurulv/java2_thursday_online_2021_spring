5d130c43e083dd453f18213032a96994a3f2a6e5
Darxney <Tresutinsandris@gmail.com>
2021-07-09 17:55:16 +0300

    RestImpl Product Complete

--- team_zero_waste_restaurant/src/main/java/lv/javaguru/java2/wasterestarant/web_ui/controllers/rest/ProductRestController.java
+++ team_zero_waste_restaurant/src/main/java/lv/javaguru/java2/wasterestarant/web_ui/controllers/rest/ProductRestController.java
@@ -1,12 +1,8 @@
 package lv.javaguru.java2.wasterestarant.web_ui.controllers.rest;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lv.javaguru.java2.wasterestarant.core.requests.product.AddProductRequest;
-import lv.javaguru.java2.wasterestarant.core.requests.product.GetProductRequest;
-import lv.javaguru.java2.wasterestarant.core.responses.product.AddProductResponse;
-import lv.javaguru.java2.wasterestarant.core.responses.product.GetProductResponse;
-import lv.javaguru.java2.wasterestarant.core.services.products.AddProductService;
-import lv.javaguru.java2.wasterestarant.core.services.products.GetProductService;
+import lv.javaguru.java2.wasterestarant.core.requests.product.*;
+import lv.javaguru.java2.wasterestarant.core.responses.product.*;
+import lv.javaguru.java2.wasterestarant.core.services.products.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +16,15 @@ public class ProductRestController {
     @Autowired
     private AddProductService addProductService;
 
+    @Autowired
+    private GetAllProductsService getAllProductsService;
+
+    @Autowired
+    private RemoveProductService removeProductService;
+
+    @Autowired
+    private SearchProductService searchProductService;
+
     @GetMapping(path = "/{id}", produces = "application/json")
     public GetProductResponse getProduct(@PathVariable Long id) {
         GetProductRequest request = new GetProductRequest(id);
@@ -30,9 +35,26 @@ public class ProductRestController {
     @PostMapping(path = "/",
             consumes = "application/json",
             produces = "application/json")
-  //@JsonFormat(pattern="dd-MM-yyyy")
     public AddProductResponse addProduct(@RequestBody AddProductRequest request) {
         return addProductService.execute(request);
+    }
+
+    @GetMapping(path = "/getAllProducts", produces = "application/json")
+    public GetAllProductsResponse response(){
+        GetAllProductsRequest request = new GetAllProductsRequest();
+        return getAllProductsService.execute(request);
+    }
+
+    @DeleteMapping(path = "/{productNameToRemove}", produces = "application/json")
+    public RemoveProductResponse deleteProduct(@PathVariable String productNameToRemove) {
+        RemoveProductRequest request = new RemoveProductRequest(productNameToRemove);
+        return removeProductService.execute(request);
+    }
+
+    @GetMapping(path = "/search/{name}", produces = "application/json")
+    public SearchProductResponse searchProductsResponse (@PathVariable String name) {
+        SearchProductRequest request = new SearchProductRequest(name);
+        return searchProductService.execute(request);
     }
 
 }
