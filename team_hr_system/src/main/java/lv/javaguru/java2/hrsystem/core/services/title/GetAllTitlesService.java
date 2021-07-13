9c332fde21fcd6a6d046b0356ad2a86bdced1195
kaverski <lena.kaverski@gmail.com>
2021-07-13 14:16:32 +0300

    rest: employees and titles

--- team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/services/title/GetAllTitlesService.java
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/services/title/GetAllTitlesService.java
@@ -6,10 +6,12 @@ import lv.javaguru.java2.hrsystem.core.requests.GetAllTitlesRequest;
 import lv.javaguru.java2.hrsystem.core.responses.GetAllTitlesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Component
+@Transactional
 public class GetAllTitlesService {
 
     @Autowired
--- team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/web_ui/controllers/GetAllEmployeesController.java
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/web_ui/controllers/GetAllEmployeesController.java
@@ -1,6 +1,5 @@
 package lv.javaguru.java2.hrsystem.web_ui.controllers;
 
-import lv.javaguru.java2.hrsystem.core.requests.AddEmployeeRequest;
 import lv.javaguru.java2.hrsystem.core.requests.GetAllEmployeesRequest;
 import lv.javaguru.java2.hrsystem.core.responses.GetAllEmployeesResponse;
 import lv.javaguru.java2.hrsystem.core.services.employee.GetAllEmployeesService;
