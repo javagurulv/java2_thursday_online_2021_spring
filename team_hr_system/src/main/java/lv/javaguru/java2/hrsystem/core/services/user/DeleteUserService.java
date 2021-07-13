051c88af6ff32c35283b9f0afcaffa8fed5b3a99
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-07-13 20:14:07 +0300

    refactoring, added validation in services

--- team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/services/user/DeleteUserService.java
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/services/user/DeleteUserService.java
@@ -2,16 +2,26 @@ package lv.javaguru.java2.hrsystem.core.services.user;
 
 import lv.javaguru.java2.hrsystem.core.database.ORMUserRepository;
 import lv.javaguru.java2.hrsystem.core.requests.DeleteUserRequest;
-import lv.javaguru.java2.hrsystem.core.responses.DeleteUserResponse;
+import lv.javaguru.java2.hrsystem.core.responses.*;
+import lv.javaguru.java2.hrsystem.core.services.validators.user.DeleteUserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class DeleteUserService {
 
     @Autowired ORMUserRepository ormUserRepository;
+    @Autowired DeleteUserValidator validator;
 
     public DeleteUserResponse execute(DeleteUserRequest deleteUserRequest) {
+
+        List<CoreError> errors = validator.validate(deleteUserRequest);
+
+        if (!errors.isEmpty()) {
+            return new DeleteUserResponse(errors);
+        }
 
         boolean deleted = ormUserRepository.deleteUser(deleteUserRequest.getId());
         return new DeleteUserResponse(deleted);
--- team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/services/user/UpdateUserRoleService.java
+++ team_hr_system/src/main/java/lv/javaguru/java2/hrsystem/core/services/user/UpdateUserRoleService.java
@@ -2,19 +2,29 @@ package lv.javaguru.java2.hrsystem.core.services.user;
 
 import lv.javaguru.java2.hrsystem.core.database.ORMUserRepository;
 import lv.javaguru.java2.hrsystem.core.requests.UpdateUserRoleRequest;
-import lv.javaguru.java2.hrsystem.core.responses.UpdateUserRoleResponse;
+import lv.javaguru.java2.hrsystem.core.responses.*;
+import lv.javaguru.java2.hrsystem.core.services.validators.user.UpdateUserRoleValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class UpdateUserRoleService {
 
     @Autowired ORMUserRepository ormUserRepository;
+    @Autowired UpdateUserRoleValidator validator;
 
-    public UpdateUserRoleResponse execute(UpdateUserRoleRequest updateUserRoleRequest) {
+    public UpdateUserRoleResponse execute(UpdateUserRoleRequest request) {
 
-       return new UpdateUserRoleResponse (ormUserRepository.updateUserRole(updateUserRoleRequest.getId(),
-               updateUserRoleRequest.getUserRole()));
+        List<CoreError> errors = validator.validate(request);
+
+        if (!errors.isEmpty()) {
+            return new UpdateUserRoleResponse(errors);
+        }
+
+        return new UpdateUserRoleResponse(ormUserRepository.updateUserRole(request.getId(),
+               request.getUserRole()));
     }
 
 }
