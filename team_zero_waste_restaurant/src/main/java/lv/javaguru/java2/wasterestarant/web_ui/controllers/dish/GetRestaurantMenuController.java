7a6d8d5e00adeab7b45e4595a5240250bfa31fa2
sinnaty <nataliasin@gmail.com>
2021-07-15 17:39:47 +0300

    zero_waste_restaurant - update for restaurant menu

---
+++ team_zero_waste_restaurant/src/main/java/lv/javaguru/java2/wasterestarant/web_ui/controllers/dish/GetRestaurantMenuController.java
@@ -0,0 +1,25 @@
+package lv.javaguru.java2.wasterestarant.web_ui.controllers.dish;
+
+import lv.javaguru.java2.wasterestarant.core.requests.GetRestaurantMenuRequest;
+import lv.javaguru.java2.wasterestarant.core.responses.GetRestaurantMenuResponse;
+import lv.javaguru.java2.wasterestarant.core.services.GetRestaurantMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GetRestaurantMenuController {
+
+    @Autowired
+    private GetRestaurantMenuService getRestaurantMenuService;
+
+    @GetMapping(value = "/getRestaurantMenu")
+    public String getRestaurantMenu(ModelMap modelMap) {
+        GetRestaurantMenuResponse response = getRestaurantMenuService.execute(new GetRestaurantMenuRequest());
+
+        modelMap.addAttribute("dishes", response.getRestaurantMenu());
+        return "/getRestaurantMenu";
+    }
+
+}
